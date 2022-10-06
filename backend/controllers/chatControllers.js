286cfb5f3795453fcdced1738ad0dee28098568e
Pajamajoker <46530662+Pajamajoker@users.noreply.github.com>
2022-10-06 14:01:41 +0530

    Group chat create and update (add and remove users) api created

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Chat = require("../models/chatModel");
+const User = require("../models/userModel");
 
 //@description     Create or fetch One to One Chat
 //@route           POST /api/chat/
@@ -12,13 +13,20 @@ const accessChat = asyncHandler(async (req, res) => {
     return res.sendStatus(400);
   }
 
-  const isChat = await Chat.find({
+  var isChat = await Chat.find({
     isGroupChat: false,
     $and: [
       { users: { $elemMatch: { $eq: req.user._id } } },
       { users: { $elemMatch: { $eq: userId } } },
     ],
-  }).populate("users", "name pic");
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
 
   if (isChat.length > 0) {
     res.send(isChat[0]);
@@ -33,7 +41,7 @@ const accessChat = asyncHandler(async (req, res) => {
       const createdChat = await Chat.create(chatData);
       const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
         "users",
-        "name pic"
+        "-password"
       );
       res.status(200).json(FullChat);
     } catch (error) {
@@ -43,4 +51,117 @@ const accessChat = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { accessChat };
+//@description     Fetch all chats for a user
+//@route           GET /api/chat/
+//@access          Protected
+const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+//@description     Create New Group Chat
+//@route           POST /api/chat/group
+//@access          Protected
+const createGroupChat = asyncHandler(async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "Please Fill all the feilds" });
+  }
+
+  var users = JSON.parse(req.body.users);
+
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .json({ message: "More than 2 users are required to form a group chat" });
+  }
+
+  users.push(req.user);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(200).json(fullGroupChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+// @desc    Remove user from Group
+// @route   PUT /api/chat/groupremove
+// @access  Protected
+const removeFromGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!removed) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(removed);
+  }
+});
+
+// @desc    Remove user from Group
+// @route   PUT /api/chat/groupadd
+// @access  Protected
+const addToGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!added) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(added);
+  }
+});
+
+module.exports = {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  removeFromGroup,
+  addToGroup,
+};
--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -1,23 +1,58 @@
+import { Box } from "@chakra-ui/layout";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
 import Chatbox from "../components/Chatbox";
 import MyChats from "../components/MyChats";
-import SideDrawer from "../components/SideDrawer";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
 
 const Chatpage = () => {
+  const history = useHistory();
+  const [user, setUser] = useState();
+  const [chats, setChats] = useState();
+  const [fetchAgain, setFetchAgain] = useState(false);
+
+  const [selectedChat, setSelectedChat] = useState();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+    // console.log(userInfo);
+
+    if (!userInfo) history.push("/");
+  }, [history]);
+
   return (
     <div style={{ width: "100%" }}>
-      <SideDrawer />
-      <div
-        style={{
-          width: "100%",
-          height: "91.5vh",
-          display: "flex",
-          justifyContent: "space-between",
-          padding: 10,
-        }}
-      >
-        <MyChats />
-        <Chatbox />
-      </div>
+      {user && (
+        <SideDrawer
+          user={user}
+          setSelectedChat={setSelectedChat}
+          setChats={setChats}
+          chats={chats}
+        />
+      )}
+      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && (
+          <MyChats
+            user={user}
+            chats={chats}
+            setChats={setChats}
+            selectedChat={selectedChat}
+            setSelectedChat={setSelectedChat}
+            fetchAgain={fetchAgain}
+          />
+        )}
+        {user && (
+          <Chatbox
+            setSelectedChat={setSelectedChat}
+            selectedChat={selectedChat}
+            user={user}
+            fetchAgain={fetchAgain}
+            setFetchAgain={setFetchAgain}
+          />
+        )}
+      </Box>
     </div>
   );
 };
